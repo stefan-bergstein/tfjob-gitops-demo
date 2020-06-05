6ff3121d6bbb6b31aa53bf5af3dfbfef81fbea43
stefan-bergstein <stefan.bergstein@gmail.com>
2020-06-05 23:02:44 +0200

    initial alpha

--- create-tfjob.sh
+++ create-tfjob.sh
@@ -3,13 +3,13 @@
 
 mkdir -p jobs
 
-NOW=`date '+%F_%H-%M-%S'`
+NOW=`date '+%F-%H-%M-%S'`
 
-echo "* Create job tf_job_mnist-${NOW}"
-sed "s|name: \"mnist\"|name: mnist-${NOW}|" tf_job_mnist.yaml > jobs/tf_job_mnist-${NOW}.yaml
+echo "* Create job tf-job-mnist-${NOW}"
+sed "s|name: \"mnist\"|name: mnist-${NOW}|" tf_job_mnist.yaml  > jobs/tf-job-mnist-${NOW}.yaml
 
-echo "* Checkin tf_job_mnist-${NOW}"
-git add jobs/tf_job_mnist-${NOW}.yaml
-git commit -m tf_job_mnist-${NOW}
+echo "* Checkin tf-job-mnist-${NOW}"
+git add jobs/tf-job-mnist-${NOW}.yaml
+git commit -m tf-job-mnist-${NOW}
 git push
 
